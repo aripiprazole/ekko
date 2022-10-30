8622118832e55cbe3a60647f3962eb296861034f
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-10-30 16:40:33 -0300

    build: update ktlint version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-  implementation("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
+  implementation("org.jlleitschuh.gradle:ktlint-gradle:11.0.0")
   implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
 }
